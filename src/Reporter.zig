5ada00d0fdac37aedb92d218efecab123ce53326
Mitchell Kember <mk12360@gmail.com>
2023-06-01 18:54:20 -0700

    Redoing some work

--- src/Reporter.zig
+++ src/Reporter.zig
@@ -4,35 +4,57 @@ const std = @import("std");
 const testing = std.testing;
 const Reporter = @This();
 
-filename: []const u8 = "<input>",
-out: ?*std.ArrayList(u8) = null, // defaults to using std.log.err
+length: usize = 0,
+buffer: [256]u8 = undefined,
 
 pub const Location = struct {
     line: u16 = 1,
     column: u16 = 1,
 };
 
-pub const Error = error{ErrorWasReported} || std.mem.Allocator.Error;
+pub const Error = error{ErrorWasReported};
 
-pub fn fail(self: Reporter, location: Location, comptime format: []const u8, args: anytype) Error {
+pub fn fail(
+    self: *Reporter,
+    filename: []const u8,
+    location: Location,
+    comptime format: []const u8,
+    args: anytype,
+) Error {
     const full_format = "{s}:{}:{}: " ++ format;
-    const full_args = .{ self.filename, location.line, location.column } ++ args;
+    const full_args = .{ filename, location.line, location.column } ++ args;
     if (@inComptime())
         @compileError(std.fmt.comptimePrint(full_format, full_args));
-    if (self.out) |out|
-        try std.fmt.format(out.writer(), full_format, full_args)
-    else
-        std.log.err(full_format, full_args);
+    if (std.fmt.bufPrint(&self.buffer, full_format, full_args)) |result| {
+        self.length = result.len;
+    } else |err| switch (err) {
+        error.NoSpaceLeft => {
+            const truncated = " (truncated)";
+            @memcpy(self.buffer[self.buffer.len - truncated.len ..], truncated);
+        },
+    }
     return error.ErrorWasReported;
 }
 
+pub fn message(self: *const Reporter) []const u8 {
+    return self.buffer[0..self.length];
+}
+
+pub fn expectFailure(self: *const Reporter, expected: []const u8, result: anytype) !void {
+    try testing.expectEqualStrings(expected, self.message());
+    try testing.expectError(error.ErrorWasReported, result);
+}
+
+test "no failure" {
+    const reporter = Reporter{};
+    try testing.expectEqualStrings("", reporter.message());
+}
+
 test "fail" {
-    var log = std.ArrayList(u8).init(testing.allocator);
-    defer log.deinit();
-    var reporter = Reporter{ .filename = "test.txt", .out = &log };
+    var reporter = Reporter{};
     try testing.expectEqual(
-        Error.ErrorWasReported,
-        reporter.fail(.{ .line = 42, .column = 5 }, "foo: {s}", .{"bar"}),
+        error.ErrorWasReported,
+        reporter.fail("test.txt", .{ .line = 42, .column = 5 }, "foo: {s}", .{"bar"}),
     );
-    try testing.expectEqualStrings("test.txt:42:5: foo: bar", log.items);
+    try testing.expectEqualStrings("test.txt:42:5: foo: bar", reporter.message());
 }
