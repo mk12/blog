dc3cad25448b5cd1c631d0ea389f6637f4503191
Mitchell Kember <mk12360@gmail.com>
2023-08-05 19:47:15 -0700

    Hello world markdown rendering

--- src/Markdown.zig
+++ src/Markdown.zig
@@ -72,32 +72,26 @@ const Token = struct {
 const TokenValue = union(enum) {
     // Blocks tokens
     newline,
-    block_html: []const u8,
-    // TODO: This can't work, blockquotes can contain fenced code.
-    // And even apart from that, scanning for the closing ``` only to rewind
-    // and higlight words within is not optimal.
-    // Instead, after seeing the opening ``` or `, descend directly into
-    // the code highlihgting scanning (NB this means it needs to recognize the
-    // closing ` for inlines, and for block it needs to yield back on every
-    // newline so we can check for blockquote > markers etc.)
-    fenced_code: struct { language: []const u8, code: []const u8 },
+    // block_html: []const u8,
     @"#": u8,
     @"-",
     @"1.",
     @">",
     @"* * *",
-    @"[^x]: ": []const u8,
-    @"$$x$$": []const u8,
+    // @"```x": []const u8,
+    // @"```",
+    // @"$$",
+    // @"[^x]: ": []const u8,
     // TODO: figures, tables, ::: verse
 
     // Inline tokens
     text: []const u8,
     // inline_html: []const u8,
-    // @"`x`": []const u8,
-    // @"$x$": []const u8,
+    // @"`",
+    // @"$",
     // @"[^x]": []const u8,
     // @"_",
-    // @"**",
+    @"**",
     // @"[",
     // @"](x)": []const u8,
     // @"][x]": []const u8,
@@ -319,21 +313,90 @@ pub fn oldRender(scanner: *Scanner, writer: anytype, links: LinkMap, options: Op
 // probably have to do two full passes
 // or write into buffer then copy
 
+fn Stack(comptime T: type) type {
+    return struct {
+        const Self = @This();
+        const max_depth = 8;
+        items: std.BoundedArray(T, max_depth) = .{},
+
+        fn len(self: Self) usize {
+            return self.items.len;
+        }
+
+        fn get(self: Self, i: usize) T {
+            return self.items.get(i);
+        }
+
+        fn last(self: Self) ?T {
+            return if (self.len() == 0) null else self.items.get(self.len() - 1);
+        }
+
+        fn push(self: *Self, writer: anytype, scanner: *Scanner, location: Location, item: T) !void {
+            try std.fmt.format(writer, "<{s}>", .{@tagName(item)});
+            self.items.append(item) catch |err| switch (err) {
+                error.Overflow => return scanner.failAt(location, "exceeded maximum depth ({})", .{max_depth}),
+            };
+        }
+
+        fn pop(self: *Self, writer: anytype) !void {
+            try std.fmt.format(writer, "</{s}>", .{@tagName(self.items.pop())});
+        }
+
+        fn truncate(self: *Self, writer: anytype, new_len: usize) !void {
+            while (self.items.len > new_len) try self.pop(writer);
+        }
+
+        fn pushOrPop(self: *Self, writer: anytype, scanner: *Scanner, location: Location, item: T) !void {
+            try if (self.last() == item) self.pop(writer) else self.push(writer, scanner, location, item);
+        }
+    };
+}
+
 const BlockTag = enum { p, li, h1, h2, h3, h4, h5, h6, ol, ul, blockquote };
 const InlineTag = enum { em, strong, a };
 
+fn headingTag(level: u8) BlockTag {
+    return @enumFromInt(@intFromEnum(BlockTag.h1) + level - 1);
+}
+
 pub fn render(scanner: *Scanner, writer: anytype, links: LinkMap, options: Options) !void {
-    _ = options;
     _ = links;
-    _ = writer;
-    var blocks = std.BoundedArray(BlockTag, 8){};
-    _ = blocks;
-    var inlines = std.BoundedArray(InlineTag, 8){};
-    _ = inlines;
+    var blocks = Stack(BlockTag){};
+    var inlines = Stack(InlineTag){};
     var tokenizer = Tokenizer{};
-    while (try tokenizer.next(scanner)) |token| {
-        _ = token;
+    outer: while (true) {
+        var token = try tokenizer.next(scanner) orelse break;
+        if (!options.is_inline) {
+            var open: usize = 0;
+            while (open < blocks.len()) : (open += 1) {
+                switch (blocks.get(open)) {
+                    .p, .li, .h1, .h2, .h3, .h4, .h5, .h6 => break,
+                    .ul => if (token.value != .@"-") break,
+                    .ol => if (token.value != .@"1.") break,
+                    .blockquote => if (token.value != .@">") break,
+                }
+                token = try tokenizer.next(scanner) orelse break :outer;
+            }
+            try blocks.truncate(writer, open);
+            const last = blocks.last();
+            try blocks.push(writer, scanner, token.location, if (last == .ol or last == .ul) .li else .p);
+        }
+        while (true) {
+            switch (token.value) {
+                .newline => break,
+                .@"#" => |level| try blocks.push(writer, scanner, token.location, headingTag(level)),
+                .@"-" => try blocks.push(writer, scanner, token.location, .ul),
+                .@"1." => try blocks.push(writer, scanner, token.location, .ol),
+                .@">" => try blocks.push(writer, scanner, token.location, .blockquote),
+                .@"* * *" => try writer.writeAll("<hr>"),
+                .text => |text| try writer.writeAll(text),
+                .@"**" => try inlines.pushOrPop(writer, scanner, token.location, .strong),
+            }
+            token = try tokenizer.next(scanner) orelse break :outer;
+        }
     }
+    // close all inlines
+    try blocks.truncate(writer, 0);
 }
 
 fn expectRenderSuccess(expected_html: []const u8, source: []const u8, links: LinkMap, options: Options) !void {
@@ -357,11 +420,11 @@ fn expectRenderFailure(expected_message: []const u8, source: []const u8, links: 
 }
 
 test "render empty string" {
-    // try expectRenderSuccess("", "", .{}, .{});
-    // try expectRenderSuccess("", "", .{}, .{ .is_inline = true });
+    try expectRenderSuccess("", "", .{}, .{});
+    try expectRenderSuccess("", "", .{}, .{ .is_inline = true });
 }
 
-// test "render text" {
-//     try expectRenderSuccess("<p>Hello world!</p>", "Hello world!", .{});
-//     try expectRenderSuccess("Hello world!", "Hello world!", .{ .is_inline = true });
-// }
+test "render text" {
+    try expectRenderSuccess("<p>Hello world!</p>", "Hello world!", .{}, .{});
+    try expectRenderSuccess("Hello world!", "Hello world!", .{}, .{ .is_inline = true });
+}
