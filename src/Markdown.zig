mk12/blog

commit 573d6a845241026bdeacd4476a3339d1716f9c15

(Broken) working on Highlighter

src/Markdown.zig
@@ -241,11 +241,8 @@ const Tokenizer = struct {
                 const level: u8 = @intCast(1 + scanner.consumeMany('#'));
                 if (level <= 6 and scanner.consume(' ')) return .{ .@"#" = level };
             },
-            '<' => if (scanner.next()) |c| switch (c) {
-                '/', 'a'...'z' => {
-                    if (scanner.consumeLineUntil('>') == null) return null;
-                    const ch = scanner.peek();
-                    if (!(ch == null or ch == '\n')) return null;
+            '<' => if (scanner.next()) |char| switch (char) {
+                '/', 'a'...'z' => if (std.mem.endsWith(u8, scanner.consumeUntilEol(), ">")) {
                     self.in_raw_html_block = true;
                     // We can't just return null here (as we do for raw inline HTML)
                     // because the renderer needs `in_raw_html_block` to be accurate.
@@ -260,12 +257,12 @@ const Tokenizer = struct {
             '`' => if (scanner.consumeString("``")) return .{ .@"```x\n" = scanner.consumeUntilEol() },
             '$' => if (scanner.consume('$')) {
                 // TODO: This is temporary, to avoid interpreting math as Markdown.
-                while (scanner.next()) |ch| if (ch == '$') break;
+                while (scanner.next()) |c| if (c == '$') break;
                 scanner.expect('$') catch unreachable;
                 return .{ .text = scanner.source[self.token_start..scanner.offset] };
             },
             '-' => if (scanner.consume(' ')) return .@"-",
-            '1'...'9' => while (scanner.next()) |c| switch (c) {
+            '1'...'9' => while (scanner.next()) |char| switch (char) {
                 '0'...'9' => {},
                 '.' => if (scanner.next() == ' ') return .@"1.",
                 else => break,
@@ -309,16 +306,16 @@ const Tokenizer = struct {
                 return .@"`";
             },
             '<' => {
-                if (scanner.peek()) |c| switch (c) {
-                    '/', 'a'...'z' => return if (scanner.consumeLineUntil('>')) |_| null else .@"<",
+                if (scanner.peek()) |char| switch (char) {
+                    '/', 'a'...'z' => if (scanner.consumeLineUntil('>')) |_| return null,
                     else => {},
                 };
                 return .@"<";
             },
-            '\\' => if (scanner.next()) |c| return .{ .@"\\x" = c },
+            '\\' => if (scanner.next()) |char| return .{ .@"\\x" = char },
             '$' => {
                 // TODO: This is temporary, to avoid interpreting math as Markdown.
-                while (scanner.next()) |ch| if (ch == '$') break;
+                while (scanner.next()) |c| if (c == '$') break;
                 return .{ .text = scanner.source[self.token_start..scanner.offset] };
             },
             '[' => {
@@ -332,13 +329,13 @@ const Tokenizer = struct {
                 var in_code = false;
                 var depth: usize = 1;
                 while (true) {
-                    const ch = scanner.next() orelse return null;
-                    if (ch == '\n') return null;
+                    const char = scanner.next() orelse return null;
+                    if (char == '\n') return null;
                     if (escaped) {
                         escaped = false;
                     } else if (in_code) {
-                        if (ch == '`') in_code = false;
-                    } else switch (ch) {
+                        if (char == '`') in_code = false;
+                    } else switch (char) {
                         '[' => depth += 1,
                         ']' => {
                             depth -= 1;
@@ -367,7 +364,7 @@ const Tokenizer = struct {
             ']' => {
                 if (self.link_depth == 0) return null;
                 self.link_depth -= 1;
-                if (scanner.peek()) |c| switch (c) {
+                if (scanner.peek()) |char| switch (char) {
                     '(' => _ = scanner.consumeLineUntil(')').?,
                     '[' => _ = scanner.consumeLineUntil(']').?,
                     else => {},

src/Highlighter.zig
@@ -13,13 +13,10 @@ const Reporter = @import("Reporter.zig");
 const Scanner = @import("Scanner.zig");
 const Highlighter = @This();
 
-// REMEMBER TO POP STASH
-
 active: bool = false,
 language: ?Language = null,
-first_line: bool = false,
-current_class: ?Class = null,
-pending_whitespace: ?[]const u8 = null,
+class: ?Class = null,
+pending_newlines: u32 = 0,
 
 pub const Language = enum {
     c,
@@ -38,28 +35,30 @@ const Class = enum {
     st, // string
 };
 
-fn keywords(comptime language: Language) type {
-    return std.ComptimeStringMap(void, switch (language) {
+fn isKeyword(comptime language: Language, identifier: []const u8) bool {
+    const keywords = switch (language) {
         .c => .{
-            .{"void"},
-            .{"int"},
-            .{"char"},
+            "char",
+            "int",
+            "void",
         },
         .ruby => .{
-            .{"def"},
-            .{"end"},
-            .{"test"},
+            "def",
+            "end",
+            "test",
         },
         .scheme => .{
-            .{"define"},
+            "define",
         },
-    });
+    };
+    comptime var kv_list: []const struct { []const u8 } = &.{};
+    inline for (keywords) |keyword| kv_list = kv_list ++ .{.{keyword}};
+    return std.ComptimeStringMap(void, kv_list).has(identifier);
 }
 
 pub fn begin(self: *Highlighter, writer: anytype, language: ?Language) !void {
     try writer.writeAll("<pre>\n<code>");
     self.active = true;
-    self.first_line = true;
     self.language = language;
 }
 
@@ -70,182 +69,160 @@ pub fn end(self: *Highlighter, writer: anytype) !void {
 }
 
 pub fn renderLine(self: *Highlighter, writer: anytype, scanner: *Scanner) !void {
-    assert(self.current_class == null);
-    if (!self.first_line) try writer.writeByte('\n');
-    self.first_line = false;
-    const language = self.language orelse {
-        var offset: usize = scanner.offset;
-        const end_offset = while (true) {
-            const char = scanner.next() orelse break scanner.offset;
-            const entity = switch (char) {
-                '\n' => break scanner.offset - 1,
-                '<' => "&lt;",
-                '&' => "&amp;",
-                else => continue,
-            };
-            try writer.writeAll(scanner.source[offset .. scanner.offset - 1]);
-            try writer.writeAll(entity);
-            offset = scanner.offset;
-        };
-        try writer.writeAll(scanner.source[offset..end_offset]);
-        return;
-    };
     while (true) {
-        const start = scanner.offset;
-        const char = scanner.next() orelse break;
-        // Doing all this in renderLine instead of separtae next() method
-        // so that it can just call self.write repeatedly if necessary, for
-        // entities and for escapes etc.
-        // Trying to do lanague-general things first, then some language specific
-        // after.
-        // NOT following the "find start after" text model, should be able to
-        // identify each token one after the other. Unlike markdown where end
-        // of text is purely defined as the start of nontext.
-
-        // NEW IDEA
-        // =========
-        // instead of calling next() which returns class (doesn't work for strings with entities, escapes)
-        // how about calling function which may write 1+ times, and do that in a loop
-        switch (char) {
-            '\n' => break,
-            '<' => try self.write(writer, "&lt;", null),
-            '&' => try self.write(writer, "&amp;", null),
-            '0'...'9' => {
-                while (scanner.peek()) |ch| switch (ch) {
-                    '0'...'9', '.', '_' => scanner.eat(),
-                    else => break,
-                };
-                try self.write(writer, scanner.source[start..scanner.offset], .cn);
-            },
-            ' ' => {
-                while (scanner.peek()) |ch| switch (ch) {
-                    ' ' => scanner.eat(),
-                    else => break,
-                };
-                try self.writeWhitespace(scanner.source[start..scanner.offset]);
-            },
-            '"' => {
-                var escape = false;
-                while (scanner.next()) |ch| {
-                    if (escape) {
-                        escape = false;
-                        continue;
-                    }
-                    switch (ch) {
-                        '\\' => escape = true,
-                        '"' => break,
-                        '&' => unreachable,
-                        '<' => unreachable,
-                        else => {},
-                    }
-                } else {
-                    return scanner.fail("unterminated string literal", .{});
-                }
-                try self.write(writer, scanner.source[start..scanner.offset], .st);
-            },
-            '\'' => {
-                // TODO scheme, don't do this
-                var escape = false;
-                while (scanner.next()) |ch| {
-                    if (escape) {
-                        escape = false;
-                        continue;
-                    }
-                    switch (ch) {
-                        '\\' => escape = true,
-                        '\'' => break,
-                        '&' => return scanner.fail("entity in string not handled", .{}),
-                        '<' => return scanner.fail("entity in string not handled", .{}),
-                        else => {},
-                    }
-                } else {
-                    return scanner.fail("unterminated string literal", .{});
-                }
-                try self.write(writer, scanner.source[start..scanner.offset], switch (language) {
-                    .ruby => .st,
-                    .c => .cn,
-                    // TODO remove
-                    .scheme => null,
-                });
-            },
-            'a'...'z', 'A'...'Z' => {
-                while (scanner.peek()) |ch| switch (ch) {
-                    'a'...'z', 'A'...'Z', '_' => scanner.eat(),
-                    '-' => if (language == .scheme) scanner.eat() else break,
-                    else => break,
-                };
-                const text = scanner.source[start..scanner.offset];
-                const kw = switch (language) {
-                    inline else => |lang| keywords(lang).has(text),
-                };
-                try self.write(writer, text, if (kw) .kw else null);
-            },
-            '/' => {
-                if (language == .c and scanner.peek() == '/') {
-                    while (scanner.peek()) |ch| switch (ch) {
-                        '\n' => break,
-                        else => scanner.eat(),
-                    };
-                    try self.write(writer, scanner.source[start..scanner.offset], .co);
-                } else {
-                    // TODO remove
-                    try self.write(writer, scanner.source[start..scanner.offset], null);
-                }
-            },
-            '#' => {
-                if (language == .ruby) {
-                    scanner.offset -= 1;
-                    break try self.write(writer, scanner.consumeUntilEol(), .co);
-                } else {
-                    // TODO remove
-                    try self.write(writer, scanner.source[start..scanner.offset], null);
-                }
-            },
-            // TODO handle @, : in ruby
-            '(', ')', ',', '*', '.', '-', '+', '=', ':', ';', '{', '}', '[', ']', '@', '?' => try self.write(writer, scanner.source[start..scanner.offset], null),
-            else => return scanner.fail("highlighter encountered unexpected character: '{c}'", .{char}),
-            // TODO one char at a time like this maybe not ideal?
-            // maybe indicates actually text-in-between model is right?
-            // else => try self.write(writer, scanner.source[start..scanner.offset], .none),
+        const status = try self.renderPrefix(writer, scanner);
+        self.pending_to = scanner.offset;
+        switch (status) {
+            .wrote => self.pending_from = scanner.offset,
+            .did_not_write => {},
+            .end_of_line => break,
         }
     }
-    try self.flush(writer);
 }
 
-fn writeWhitespace(self: *Highlighter, whitespace: []const u8) !void {
-    assert(self.pending_whitespace == null);
-    self.pending_whitespace = whitespace;
+const Status = enum { wrote, did_not_write, end_of_line };
+
+fn renderPrefix(self: *Highlighter, writer: anytype, scanner: *Scanner) !Status {
+    const language = self.language;
+    const start = scanner.offset;
+    const char = scanner.next() orelse return .end_of_line;
+    switch (char) {
+        '\n' => return .end_of_line,
+        '<' => return self.write(writer, "&lt;", null),
+        '&' => return self.write(writer, "&amp;", null),
+        '0'...'9' => {
+            while (scanner.peek()) |c| switch (c) {
+                '0'...'9', '.', '_' => scanner.eat(c),
+                else => break,
+            };
+            return self.write(writer, scanner.source[start..scanner.offset], .cn);
+        },
+        ' ' => {
+            while (scanner.peek()) |c| switch (c) {
+                ' ' => scanner.eat(),
+                else => break,
+            };
+            try self.writeWhitespace(scanner.source[start..scanner.offset]);
+        },
+        '"' => {
+            var escape = false;
+            while (scanner.next()) |c| {
+                if (escape) {
+                    escape = false;
+                    continue;
+                }
+                switch (c) {
+                    '\\' => escape = true,
+                    '"' => break,
+                    '&' => unreachable,
+                    '<' => unreachable,
+                    else => {},
+                }
+            } else {
+                return scanner.fail("unterminated string literal", .{});
+            }
+            try self.write(writer, scanner.source[start..scanner.offset], .st);
+        },
+        '\'' => {
+            // TODO scheme, don't do this
+            var escape = false;
+            while (scanner.next()) |c| {
+                if (escape) {
+                    escape = false;
+                    continue;
+                }
+                switch (c) {
+                    '\\' => escape = true,
+                    '\'' => break,
+                    '&' => return scanner.fail("entity in string not handled", .{}),
+                    '<' => return scanner.fail("entity in string not handled", .{}),
+                    else => {},
+                }
+            } else {
+                return scanner.fail("unterminated string literal", .{});
+            }
+            try self.write(writer, scanner.source[start..scanner.offset], switch (language) {
+                .ruby => .st,
+                .c => .cn,
+                // TODO remove
+                .scheme => null,
+            });
+        },
+        'a'...'z', 'A'...'Z' => {
+            while (scanner.peek()) |c| switch (c) {
+                'a'...'z', 'A'...'Z', '_' => scanner.eat(),
+                '-' => if (language == .scheme) scanner.eat() else break,
+                else => break,
+            };
+            const text = scanner.source[start..scanner.offset];
+            const kw = switch (language) {
+                inline else => |lang| isKeyword(lang, text),
+            };
+            try self.write(writer, text, if (kw) .kw else null);
+        },
+        '/' => {
+            if (language == .c and scanner.peek() == '/') {
+                while (scanner.peek()) |c| switch (c) {
+                    '\n' => break,
+                    else => scanner.eat(),
+                };
+                try self.write(writer, scanner.source[start..scanner.offset], .co);
+            } else {
+                // TODO remove
+                try self.write(writer, scanner.source[start..scanner.offset], null);
+            }
+        },
+        '#' => {
+            if (language == .ruby) {
+                scanner.offset -= 1;
+                try self.write(writer, scanner.consumeUntilEol(), .co);
+                return .end_of_line;
+            } else {
+                // TODO remove
+                try self.write(writer, scanner.source[start..scanner.offset], null);
+            }
+        },
+        else => return .did_not_write,
+        // TODO handle @, : in ruby
+        // '(', ')', ',', '*', '.', '-', '+', '=', ':', ';', '{', '}', '[', ']', '@', '?' => try self.write(writer, scanner.source[start..scanner.offset], null),
+        // else => return scanner.fail("highlighter encountered unexpected character: '{c}'", .{char}),
+    }
 }
 
-fn write(self: *Highlighter, writer: anytype, text: []const u8, maybe_class: ?Class) !void {
-    if (maybe_class != self.current_class) {
-        try self.flush(writer);
-        if (maybe_class) |class| {
-            try fmt.format(writer, "<span class=\"{s}\">", .{@tagName(class)});
-            self.current_class = class;
-        }
+fn write(self: *Highlighter, writer: anytype, text: []const u8, class: ?Class) !Status {
+    if (class == self.class and self.pending_is_space) {
+        try self.flushPending(writer);
     } else {
-        try self.flushWhitespace(writer);
+        try self.flush(writer);
+        if (class) |c| try fmt.format(writer, "<span class=\"{s}\">", .{@tagName(c)});
+        self.class = class;
     }
     try writer.writeAll(text);
+    return .wrote;
+}
+
+fn writeWhitespace(self: *Highlighter, writer: anytype, text: []const u8) !void {
+    _ = text;
+    _ = writer;
+    _ = self;
 }
 
 fn flush(self: *Highlighter, writer: anytype) !void {
     try self.flushSpanTag(writer);
-    try self.flushWhitespace(writer);
+    try self.flushPending(writer);
 }
 
 fn flushSpanTag(self: *Highlighter, writer: anytype) !void {
-    if (self.current_class) |_| {
+    if (self.class) |_| {
         try writer.writeAll("</span>");
-        self.current_class = null;
+        self.class = null;
     }
 }
 
-fn flushWhitespace(self: *Highlighter, writer: anytype) !void {
-    if (self.pending_whitespace) |space| {
-        try writer.writeAll(space);
-        self.pending_whitespace = null;
+fn flushPending(self: *Highlighter, writer: anytype) !void {
+    if (self.pending) |text| {
+        try writer.writeAll(text);
+        self.pending = null;
     }
 }
 
@@ -275,6 +252,22 @@ test "one line" {
 
 test "escape with entities" {
     try expectHighlight("<pre>\n<code>&lt;&amp;></code>\n</pre>", "<&>", null);
+}
+
+test "two keywords in a row" {
+    try expectHighlight("<pre>\n<code><span class=\"kw\">def def</span></code>\n</pre>", "def def", .ruby);
+}
+
+test "two keywords on separate lines" {
+    try expectHighlight("<pre>\n<code><span class=\"kw\">def\ndef</span></code>\n</pre>", "def\ndef", .ruby);
+}
+
+test "two keywords on separate lines with indent" {
+    try expectHighlight("<pre>\n<code><span class=\"kw\">def\n  def</span></code>\n</pre>", "def\n  def", .ruby);
+}
+
+test "two keywords on separate lines with blank" {
+    try expectHighlight("<pre>\n<code><span class=\"kw\">def\n\ndef</span></code>\n</pre>", "def\n\ndef", .ruby);
 }
 
 test "highlight ruby" {

src/Scanner.zig
@@ -46,10 +46,10 @@ pub fn prev(self: Scanner, count: usize) ?u8 {
 }
 
 pub fn consume(self: *Scanner, char: u8) bool {
-    if (self.peek()) |c| if (c == char) {
+    if (self.peek() == char) {
         self.eat();
         return true;
-    };
+    }
     return false;
 }
 
@@ -78,9 +78,9 @@ pub fn consumeLength(self: *Scanner, length: usize) ?[]const u8 {
 
 pub fn consumeLineUntil(self: *Scanner, delimiter: u8) ?[]const u8 {
     const start = self.offset;
-    while (self.next()) |char| {
-        if (char == delimiter) return self.source[start .. self.offset - 1];
-        if (char == '\n') break;
+    while (self.next()) |c| {
+        if (c == delimiter) return self.source[start .. self.offset - 1];
+        if (c == '\n') break;
     }
     self.offset = start;
     return null;
@@ -88,7 +88,7 @@ pub fn consumeLineUntil(self: *Scanner, delimiter: u8) ?[]const u8 {
 
 pub fn consumeUntilEol(self: *Scanner) []const u8 {
     const start = self.offset;
-    while (self.next()) |ch| if (ch == '\n') return self.source[start .. self.offset - 1];
+    while (self.next()) |c| if (c == '\n') return self.source[start .. self.offset - 1];
     return self.source[start..self.offset];
 }
 

commit a7a7ee43a1acb70adc253aef6fda01793c16e760

Fix regression in raw block HTML recognition
Was introduced in 573d6a845241026bdeacd4476a3339d1716f9c15

src/Markdown.zig
@@ -242,7 +242,7 @@ const Tokenizer = struct {
                 if (level <= 6 and scanner.consume(' ')) return .{ .@"#" = level };
             },
             '<' => if (scanner.next()) |char| switch (char) {
-                '/', 'a'...'z' => if (std.mem.endsWith(u8, scanner.consumeUntilEol(), ">")) {
+                '/', 'a'...'z' => if (scanner.consumeLineUntil('>') != null and scanner.peek() == '\n' or scanner.eof()) {
                     self.in_raw_html_block = true;
                     // We can't just return null here (as we do for raw inline HTML)
                     // because the renderer needs `in_raw_html_block` to be accurate.