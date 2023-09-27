0377662f0b53daf71337cbfb4d6fc6e905507919
Mitchell Kember <mk12360@gmail.com>
2023-09-26 22:09:10 -0700

    Enable heading IDs in posts (forgot to before)

--- src/generate.zig
+++ src/generate.zig
@@ -357,7 +357,7 @@ fn generatePost(
         .title = markdown(post.meta.title, post.context, .{ .is_inline = true }),
         .subtitle = markdown(post.meta.subtitle, post.context, .{ .is_inline = true }),
         .date = date(post.meta.status, .long),
-        .content = markdown(post.body, post.context, .{ .shift_heading_level = 1, .highlight_code = true }),
+        .content = markdown(post.body, post.context, .{ .shift_heading_level = 1, .highlight_code = true, .auto_heading_ids = true }),
         .newer = try if (neighbors.newer) |newer| base_url.postUrl(allocator, newer.slug) else base_url.join(allocator, "/"),
         .older = try if (neighbors.older) |older| base_url.postUrl(allocator, older.slug) else base_url.join(allocator, "/post/"),
     });
